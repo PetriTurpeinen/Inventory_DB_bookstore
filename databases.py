64bedf16ade7eccc88a46ea93571d757c7ee1253
PetriTurpeinen <plexi20@gmail.com>
2021-11-22 15:55:14 +0200

    connection statement in databases file fixed

--- databases.py
+++ databases.py
@@ -14,9 +14,9 @@ class Databases():
 
     def connect_to_database(self,username,userpassword):        
         try:            
-            conn = pg2.connect(database='kirjakauppa', user=os.environ["DB_USER"], password=os.environ["DB_PASSWORD"])
+            conn = pg2.connect(database=os.environ["DB_DATABASE"], user=os.environ["DB_USER"], password=os.environ["DB_PASSWORD"])
             self.cur = conn.cursor()            
-            #print("Connection established to database")
+            print("Connection established to database")
         except pg2.OperationalError:
             print(("Connection error"))
         except:
@@ -27,8 +27,10 @@ class Databases():
 
         if result is None:
             print(self.UI.error_messagebox("Login name or password was incorrect"))
+            return False
         else:
-            print(self.UI.error_messagebox("User and password were right"))           
+            print(self.UI.error_messagebox("User and password were right"))
+            return True           
 
     def query_from_database(self):        
 
