5e6f69b3f99040c7b8a9fe3b655fce82f406c136
PetriTurpeinen <plexi20@gmail.com>
2021-12-02 21:59:25 +0200

    Payment transaction function added

--- databases.py
+++ databases.py
@@ -35,32 +35,34 @@ class Databases():
             return True           
 
     def query_from_database(self, title, author, category, isbn, price):
+        print(title,author,category,isbn,price)
         # A test query to get books from the database        
         title = "%" + title + "%"
         author = "%" + author + "%"
         category = "%" + category + "%"
-        isbn = "%" + isbn + "%"        
+        isbn = "%" + isbn + "%"
+        price = "%" + price + "%"        
 
         with self.conn:
             #Add column names to the query
             self.cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'kirjat'")
 
             column_names = self.cur.fetchall()
-            #print(column_names)
+            print(column_names)
 
             columns_list = []
 
             for tup in column_names:
                 columns_list += [ tup[0] ]
-            
+            #Add columns to a tuple so it can be combined with query
             data_tuple = (*columns_list,)
 
-            #print(data_tuple)          
+            #print(data_tuple)         
 
-            #self.cur.execute("SELECT * FROM kirjat")
-            self.cur.execute("SELECT * FROM kirjat WHERE nimeke LIKE %s AND tekija LIKE %s AND kategoria LIKE %s AND isbn LIKE %s AND hinta = %s", (title,author,category,isbn,price));
+            self.cur.execute("SELECT * FROM kirjat WHERE nimeke LIKE %s AND tekija LIKE %s AND kategoria LIKE %s AND isbn LIKE %s AND hinta LIKE %s ORDER BY kirjat_id ASC", (title,author,category,isbn,price));
 
             data_queries = self.cur.fetchall()
+            #print(data_queries)
             
             data_queries.append(data_queries[0])
             data_queries[0] = data_tuple
@@ -68,4 +70,37 @@ class Databases():
         #print(data)            
         
         return data_queries
+
+    def add_to_database(self,title,author,isbn,category,publisher,languages,publishyear,edition,pages,typography,condition,amount,extrainformation,price):
+        #title,author,isbn,category,publisher,languages,publishyear,edition,pages,typography,condition,amount,extrainformation,price
+        #print(title,author,isbn,category,publisher,languages,publishyear,edition,pages,typography,condition,amount,extrainformation,price)
+
+        #lista = [title,author,isbn,category,publisher,languages,publishyear,edition,pages,typography,condition,amount,extrainformation,price]
+        #print(len(lista))
+
+        
+        
+        self.cur.execute("INSERT INTO kirjat (nimeke, tekija, isbn, kategoria, kustantaja, kielet, julkaisuvuosi, painos, sivumaara, painoasu, kuntoluokka, kappalemaara, lisatiedot, hinta) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING kirjat_id;",(title,author,isbn,category,publisher,languages,publishyear,edition,pages,typography,condition,amount,extrainformation,price));
+        #self.cur.execute("INSERT INTO testi(nimi,ika) VALUES(%s, %s) RETURNING testi_id;", (title,author))
+        id = self.cur.fetchone()[0]
+        self.conn.commit()    
+
+        print(self.UI.error_messagebox(f"Title id: {id} was succesfully added to the database!"))
+    
+    def add_payment_to_database(self, title_id, price, postage, location, date):
+
+        self.cur.execute("INSERT INTO maksut (kirjat_id, hinta, toimituskulut, kaupunki, maksupvm) VALUES (%s, %s, %s, %s, %s) RETURNING maksut_id;",(title_id, price, postage, location, date));
+
+        id = self.cur.fetchone()[0]
+        self.conn.commit()    
+
+        print(self.UI.error_messagebox(f"Payment transaction: {id} was succesfully added to the database!"))
+
+        
+                
+            
+        
+        
+
+
 
--- userinterface.py
+++ userinterface.py
@@ -93,7 +93,10 @@ class UserInterface():
         helpmenu.add_command(label="About", command=self.showinfo_about)
         menubar.add_cascade(label="Help", menu=helpmenu)
         self.root.config(menu=menubar)
-    
+
+    def send_form_to_database(self):
+        self.data.add_to_database(self.tkAddTitle.get(),self.tkAddAuthor.get(),self.tkAddISBN.get(),self.tkAddCategory.get(),self.tkAddPublisher.get(),self.tkAddLanguages.get(),self.tkAddPublishYear.get(),self.tkAddEdition.get(),self.tkAddPages.get(),self.addTypography.get(),self.addCondition.get(),self.addAmount.get(),self.addExtraInformation.get(),self.tkAddPrice.get())
+        
     def queries_menu(self):        
 
         #Pop menu for queries
@@ -105,7 +108,7 @@ class UserInterface():
         frame.grid_columnconfigure(0,weight=1)
         frame.grid_rowconfigure(0,weight=1)             
   
-        table = self.data.query_from_database(self.tkTtitle.get(), self.tkAuthor.get(), self.tkCategory.get(), self.tkISBN.get(),self.tkPrice.get())
+        table = self.data.query_from_database(self.tkGetTitle.get(), self.tkGetAuthor.get(), self.tkGetCategory.get(), self.tkGetISBN.get(),self.tkGetPrice.get())
 
         sheet = tksheet.Sheet(top)
 
@@ -142,6 +145,9 @@ class UserInterface():
         sheet.grid(row = 0, column = 0, sticky = "nswe") 
   
         top.mainloop()
+    
+    def send_transactions_to_database(self):        
+        self.data.add_payment_to_database(self.tkPayTitleId.get(), self.tkPayPrice.get(), self.tkPayPostage.get(), self.tkPayLocation.get(), self.tkPayDate.get())
 
     def empty_menu(self):
         #Clear UI
@@ -175,88 +181,115 @@ class UserInterface():
 
         #Form to add/remove/modify titles
 
+        self.tkAddTitle = StringVar(self.root)
+        self.tkAddAuthor = StringVar(self.root)
+        self.tkAddISBN = StringVar(self.root)
+        self.tkAddCategory = StringVar(self.root)
+        self.tkAddPublisher = StringVar(self.root)
+        self.tkAddLanguages = StringVar(self.root)
+        self.tkAddPublishYear = StringVar(self.root)
+        self.tkAddEdition = StringVar(self.root)
+        self.tkAddPages = StringVar(self.root)
+        self.addTypography = StringVar(self.root)
+        self.addCondition = StringVar(self.root)
+        self.addAmount = StringVar(self.root)
+        self.addExtraInformation = StringVar(self.root)             
+        self.tkAddPrice = StringVar(self.root)
+
         self.remove_widgets()         
 
-        price_label = tk.Label(self.frame, text="Hinta:")
-        price_label.grid(row=0, column = 0, sticky=tk.W, padx=5, pady=5)
+        title_label = tk.Label(self.frame, text="Nimeke:")
+        title_label.grid(row=0, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        price_entry = tk.Entry(self.frame)
-        price_entry.grid(row=0, column=1, sticky=tk.E, padx=5, pady=5)
+        title_entry = tk.Entry(self.frame, textvariable=self.tkAddTitle)
+        title_entry.grid(row=0, column=1, sticky=tk.E, padx=5, pady=5)
 
-        isbn_label = tk.Label(self.frame, text="Isbn:")
-        isbn_label.grid(row=1, column = 0, sticky=tk.W, padx=5, pady=5)
+        author_label = tk.Label(self.frame, text="Tekijä:")
+        author_label.grid(row=1, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        isbn_entry = tk.Entry(self.frame)
-        isbn_entry.grid(row=1, column=1, sticky=tk.E, padx=5, pady=5)
+        author_entry = tk.Entry(self.frame, textvariable=self.tkAddAuthor)
+        author_entry.grid(row=1, column=1, sticky=tk.E, padx=5, pady=5)
 
-        publish_year_label = tk.Label(self.frame, text="Julkaisuvuosi:")
-        publish_year_label.grid(row=2, column = 0, sticky=tk.W, padx=5, pady=5)
+        ISBN_label = tk.Label(self.frame, text="ISBN:")
+        ISBN_label.grid(row=2, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        publish_year_entry = tk.Entry(self.frame)
-        publish_year_entry.grid(row=2, column=1, sticky=tk.E, padx=5, pady=5)
-
-        amount_label = tk.Label(self.frame, text="Kappalemäärä:")
-        amount_label.grid(row=3, column = 0, sticky=tk.W, padx=5, pady=5)
-
-        amount_entry = tk.Entry(self.frame)
-        amount_entry.grid(row=3, column=1, sticky=tk.E, padx=5, pady=5)
+        ISBN_entry = tk.Entry(self.frame, textvariable=self.tkAddISBN)
+        ISBN_entry.grid(row=2, column=1, sticky=tk.E, padx=5, pady=5)
 
         category_label = tk.Label(self.frame, text="Kategoria:")
-        category_label.grid(row=4, column = 0, sticky=tk.W, padx=5, pady=5)
+        category_label.grid(row=3, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        category_entry = tk.Entry(self.frame)
-        category_entry.grid(row=4, column=1, sticky=tk.E, padx=5, pady=5)
+        category_entry = tk.Entry(self.frame, textvariable=self.tkAddCategory)
+        category_entry.grid(row=3, column=1, sticky=tk.E, padx=5, pady=5)
+
+        publisher_label = tk.Label(self.frame, text="Kustantaja:")
+        publisher_label.grid(row=4, column = 0, sticky=tk.W, padx=5, pady=5)
+
+        publisher_entry = tk.Entry(self.frame, textvariable=self.tkAddPublisher)
+        publisher_entry.grid(row=4, column=1, sticky=tk.E, padx=5, pady=5)
 
         languages_label = tk.Label(self.frame, text="Kielet:")
         languages_label.grid(row=5, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        languages_entry = tk.Entry(self.frame)
+        languages_entry = tk.Entry(self.frame, textvariable=self.tkAddLanguages)
         languages_entry.grid(row=5, column=1, sticky=tk.E, padx=5, pady=5)
 
-        publisher_label = tk.Label(self.frame, text="Kustantaja:")
-        publisher_label.grid(row=6, column = 0, sticky=tk.W, padx=5, pady=5)
+        publish_year_label = tk.Label(self.frame, text="Julkaisuvuosi:")
+        publish_year_label.grid(row=6, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        publisher_entry = tk.Entry(self.frame)
-        publisher_entry.grid(row=6, column=1, sticky=tk.E, padx=5, pady=5)
+        publish_year_entry = tk.Entry(self.frame, textvariable=self.tkAddPublishYear)
+        publish_year_entry.grid(row=6, column=1, sticky=tk.E, padx=5, pady=5)
+
+        edition_label = tk.Label(self.frame, text="Painos:")
+        edition_label.grid(row=7, column = 0, sticky=tk.W, padx=5, pady=5)
+
+        edition_entry = tk.Entry(self.frame, textvariable = self.tkAddEdition)
+        edition_entry.grid(row=7, column=1, sticky=tk.E, padx=5, pady=5)
+
+        pages_label = tk.Label(self.frame, text="Sivumäärä:")
+        pages_label.grid(row=8, column = 0, sticky=tk.W, padx=5, pady=5)
+
+        pages_entry = tk.Entry(self.frame, textvariable=self.tkAddPages)
+        pages_entry.grid(row=8, column=1, sticky=tk.E, padx=5, pady=5)
+
+        typography_label = tk.Label(self.frame, text="Painoasu:")
+        typography_label.grid(row=9, column = 0, sticky=tk.W, padx=5, pady=5)
+
+        typography_entry = tk.Entry(self.frame, textvariable=self.addTypography)
+        typography_entry.grid(row=9, column=1, sticky=tk.E, padx=5, pady=5)
 
         condition_label = tk.Label(self.frame, text="Kuntoluokka:")
-        condition_label.grid(row=7, column = 0, sticky=tk.W, padx=5, pady=5)
+        condition_label.grid(row=10, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        condition_entry = tk.Entry(self.frame)
-        condition_entry.grid(row=7, column=1, sticky=tk.E, padx=5, pady=5)
+        condition_entry = tk.Entry(self.frame, textvariable=self.addCondition)
+        condition_entry.grid(row=10, column=1, sticky=tk.E, padx=5, pady=5)
+
+        amount_label = tk.Label(self.frame, text="Kappalemäärä:")
+        amount_label.grid(row=11, column = 0, sticky=tk.W, padx=5, pady=5)
+
+        amount_entry = tk.Entry(self.frame, textvariable=self.addAmount)
+        amount_entry.grid(row=11, column=1, sticky=tk.E, padx=5, pady=5)
 
         extra_information_label = tk.Label(self.frame, text="Lisätiedot:")
-        extra_information_label.grid(row=8, column = 0, sticky=tk.W, padx=5, pady=5)
+        extra_information_label.grid(row=12, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        extra_information_entry = tk.Entry(self.frame)
-        extra_information_entry.grid(row=8, column=1, sticky=tk.E, padx=5, pady=5)
+        extra_information_entry = tk.Entry(self.frame, textvariable=self.addExtraInformation)
+        extra_information_entry.grid(row=12, column=1, sticky=tk.E, padx=5, pady=5)
 
-        title_label = tk.Label(self.frame, text="Nimeke:")
-        title_label.grid(row=9, column = 0, sticky=tk.W, padx=5, pady=5)
+        price_label = tk.Label(self.frame, text="Hinta:")
+        price_label.grid(row=13, column = 0, sticky=tk.W, padx=5, pady=5)
 
-        title_entry = tk.Entry(self.frame)
-        title_entry.grid(row=9, column=1, sticky=tk.E, padx=5, pady=5)
+        price_entry = tk.Entry(self.frame, textvariable=self.tkAddPrice)
+        price_entry.grid(row=13, column=1, sticky=tk.E, padx=5, pady=5)
 
-        extra_information_label = tk.Label(self.frame, text="Painoasu:")
-        extra_information_label.grid(row=10, column = 0, sticky=tk.W, padx=5, pady=5)
-
-        extra_information_entry = tk.Entry(self.frame)
-        extra_information_entry.grid(row=10, column=1, sticky=tk.E, padx=5, pady=5)
-
-        extra_information_label = tk.Label(self.frame, text="Tekijä:")
-        extra_information_label.grid(row=11, column = 0, sticky=tk.W, padx=5, pady=5)
-
-        extra_information_entry = tk.Entry(self.frame)
-        extra_information_entry.grid(row=11, column=1, sticky=tk.E, padx=5, pady=5)
-
-        add_button = tk.Button(self.frame, text="Lisää nimeke")
-        add_button.grid(row=11, column=2, sticky=tk.E, padx=5, pady=5)
+        add_button = tk.Button(self.frame, text="Lisää nimeke", command=self.send_form_to_database)
+        add_button.grid(row=13, column=2, sticky=tk.E, padx=5, pady=5)
 
         remove_button = tk.Button(self.frame, text="Poista nimeke")
-        remove_button.grid(row=11, column=3, sticky=tk.E, padx=5, pady=5)
+        remove_button.grid(row=13, column=3, sticky=tk.E, padx=5, pady=5)
 
         modify_button = tk.Button(self.frame, text="Muokkaa nimekettä")
-        modify_button.grid(row=11, column=4, sticky=tk.E, padx=5, pady=5)
+        modify_button.grid(row=13, column=4, sticky=tk.E, padx=5, pady=5)
 
     def queries(self):
 
@@ -264,11 +297,11 @@ class UserInterface():
 
         self.remove_widgets()          
 
-        self.tkTtitle = StringVar(self.root)
-        self.tkAuthor = StringVar(self.root)
-        self.tkCategory = StringVar(self.root)
-        self.tkISBN = StringVar(self.root)
-        self.tkPrice = StringVar(self.root)
+        self.tkGetTitle = StringVar(self.root)
+        self.tkGetAuthor = StringVar(self.root)
+        self.tkGetCategory = StringVar(self.root)
+        self.tkGetISBN = StringVar(self.root)
+        self.tkGetPrice = StringVar(self.root)
 
         #QUERY FORMS
                 
@@ -300,19 +333,19 @@ class UserInterface():
         #optionmenu_category2_criteria = ttk.OptionMenu(self.frame,tkvar3,options_categories2_criteria[0], *options_categories2_criteria)
         #optionmenu_category2_criteria.grid(row=1,column=1, padx=(5,5),pady=0)
    
-        optionmenu_title_entry = tk.Entry(self.frame, width=15, textvariable=self.tkTtitle)
+        optionmenu_title_entry = tk.Entry(self.frame, width=15, textvariable=self.tkGetTitle)
         optionmenu_title_entry.grid(row=0, column=1)
 
-        optionmenu_author_entry = tk.Entry(self.frame, width=15, textvariable=self.tkAuthor)
+        optionmenu_author_entry = tk.Entry(self.frame, width=15, textvariable=self.tkGetAuthor)
         optionmenu_author_entry.grid(row=1, column=1)
 
-        optionmenu_category_entry = tk.Entry(self.frame, width=15, textvariable=self.tkCategory)
+        optionmenu_category_entry = tk.Entry(self.frame, width=15, textvariable=self.tkGetCategory)
         optionmenu_category_entry.grid(row=2, column=1)
 
-        optionmenu_ISBN_entry = tk.Entry(self.frame, width=15, textvariable=self.tkISBN)
+        optionmenu_ISBN_entry = tk.Entry(self.frame, width=15, textvariable=self.tkGetISBN)
         optionmenu_ISBN_entry.grid(row=3, column=1)
 
-        optionmenu_price_entry = tk.Entry(self.frame, width=15, textvariable=self.tkPrice)
+        optionmenu_price_entry = tk.Entry(self.frame, width=15, textvariable=self.tkGetPrice)
         optionmenu_price_entry.grid(row=4, column=1)
 
         optionmenu_startdate_entry = tk.Entry(self.frame, width=15)
@@ -334,30 +367,54 @@ class UserInterface():
     def payment_transactions(self):
         #Add payment transactions
 
+        self.tkPayTitleId = StringVar(self.root)
+        self.tkPayPrice = StringVar(self.root)
+        self.tkPayPostage = StringVar(self.root)
+        self.tkPayLocation = StringVar(self.root)
+        self.tkPayDate = StringVar(self.root)   
+
         self.remove_widgets()        
   
         self.frame2.grid(row = 2, column = 0, sticky = W+E+N+S,padx=10, pady=10)
 
-        nimeke_id_label = tk.Label(self.frame, text="Nimeke id:")
-        nimeke_id_label.grid(row=0, column=0, padx=5, pady=5)
+        title_id_label = tk.Label(self.frame, text="Nimeke id:")
+        title_id_label.grid(row=0, column=0, padx=5, pady=5)
 
-        nimeke_id_entry = tk.Entry(self.frame)
-        nimeke_id_entry.grid(row=0, column=1, padx=5,pady=5)
+        title_id_entry = tk.Entry(self.frame, textvariable=self.tkPayTitleId)
+        title_id_entry.grid(row=0, column=1, padx=5,pady=5)
 
-        payment_date_label = tk.Label(self.frame, text="Päivämäärä:")
-        payment_date_label.grid(row=1, column=0, padx=5, pady=5)
+        price_date_label = tk.Label(self.frame, text="Hinta:")
+        price_date_label.grid(row=1, column=0, padx=5, pady=5)
 
-        payment_date_entry = tk.Entry(self.frame)
-        payment_date_entry.grid(row=1, column=1, padx=5,pady=5)
+        price_date_entry = tk.Entry(self.frame,textvariable=self.tkPayPrice)
+        price_date_entry.grid(row=1, column=1, padx=5,pady=5)
 
-        payment_add_button = tk.Button(self.frame, text="Lisää maksutapahtuma")
-        payment_add_button.grid(row=1, column=2, padx=5, pady=5)
+        postage_label = tk.Label(self.frame, text="Toimituskulut:")
+        postage_label.grid(row=2, column=0, padx=5, pady=5)
+
+        postage_entry = tk.Entry(self.frame,textvariable=self.tkPayPostage)
+        postage_entry.grid(row=2, column=1, padx=5,pady=5)
+
+        location_label = tk.Label(self.frame, text="Paikkakunta:")
+        location_label.grid(row=3, column=0, padx=5, pady=5)
+
+        location_entry = tk.Entry(self.frame,textvariable=self.tkPayLocation)
+        location_entry.grid(row=3, column=1, padx=5,pady=5)
+
+        payment_date_label = tk.Label(self.frame, text="Maksupvm:")
+        payment_date_label.grid(row=4, column=0, padx=5, pady=5)
+
+        payment_date_entry = tk.Entry(self.frame,textvariable=self.tkPayDate)
+        payment_date_entry.grid(row=4, column=1, padx=5,pady=5)
+
+        payment_add_button = tk.Button(self.frame, text="Lisää maksutapahtuma", command=self.send_transactions_to_database)
+        payment_add_button.grid(row=4, column=2, padx=5, pady=5)
 
         payment_modify_button = tk.Button(self.frame, text="Muokkaa maksutapahtumaa")
-        payment_modify_button.grid(row=1, column=3, padx=5, pady=5)
+        payment_modify_button.grid(row=4, column=3, padx=5, pady=5)
 
         payment_delete_button = tk.Button(self.frame, text="Poista maksutapahtuma")
-        payment_delete_button.grid(row=1, column=4, padx=5, pady=5)
+        payment_delete_button.grid(row=4, column=4, padx=5, pady=5)
 
         #Get payment transactions
 
